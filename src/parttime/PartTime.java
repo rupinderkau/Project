442daa65d958d03070d22f3c355604bd99bd3e3f
rupinderkau <rupinder.366@gmail.com>
2018-06-06 12:14:08 +1200

    First Time

---
+++ src/parttime/PartTime.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parttime;
+
+
+public class PartTime {
+
+    int RateHoursWorked;
+    public static void main(String[] args) {
+        
+    }
+    
+}
